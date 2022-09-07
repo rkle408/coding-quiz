91ee7de80db53fd52439d112b1ca7e86d6119a0c
Rosebud <rkle408@gmail.com>
2022-09-06 21:25:31 -0700

    revert answers style, start variables in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 var startButton = document.querySelector(".start-button");
+var goBackButton = document.querySelector(".go-back");
+var clearButton = document.querySelector(".clear-scores");
 var timerElement = document.querySelector(".timer");
+
+var timer;
+var timerCount;
+
+// Will probablly need more variables as we go
+
+// Need a start function for when we click the start button for the quiz and timers to start
+function startQuiz() {
+    timerCount = 80;
+    startButton.disabled = true;
+    startTimer();
+}
