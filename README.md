4a46bcbd7e03266136b1c874964b5a65f6a9a1bb
Rosebud <rkle408@gmail.com>
2022-09-06 21:39:52 -0700

    start questions array, re-id buttons

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 
 
 ## Resources:
-- Lemonaki, D. "CSS Button Style – Hover, Color, and Background." <b>FreeCodeCamp</b>. <https://www.freecodecamp.org/news/css-button-style-hover-color-and-background/>. Date Accessed: Sept. 6, 2022
+- Lemonaki, D. "CSS Button Style – Hover, Color, and Background." <b>FreeCodeCamp</b>. <https://www.freecodecamp.org/news/css-button-style-hover-color-and-background/>. Date Accessed: Sept. 6, 2022.
+- "Create a Quiz App using HTML CSS & JavaScript | Quiz Web App using JavaScript." <b>Coding with Nick</b>. <https://www.youtube.com/watch?v=CqddbIrEM5I>. Date Accessed: Sept. 6, 2022.
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,58 @@ var timerCount;
 
 // Will probablly need more variables as we go
 
+// Quiz Questions
+var quizQuestions = [
+    {
+        question: "What are functions inside of objects called?",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }
+    {
+        question: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }
+    {
+        question: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }    
+    {
+        question: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }
+    {
+        question: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }
+    {
+        question: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        correct: "",
+    }
+]
+
 // Need a start function for when we click the start button for the quiz and timers to start
 function startQuiz() {
     timerCount = 80;
